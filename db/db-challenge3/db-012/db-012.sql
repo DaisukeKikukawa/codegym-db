add09a62cf22c3d06a8b6c501d233644af48b285
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-06-07 21:42:02 +0900

    db-012 SQL文 不要スペースの削除

--- db/db-challenge3/db-012/db-012.sql
+++ db/db-challenge3/db-012/db-012.sql
@@ -1,14 +1,14 @@
 BEGIN;
-UPDATE 
+UPDATE
       chatrooms
 JOIN
       chatroom_join_users
 ON
       chatrooms.id = chatroom_join_users.chatroom_id
-SET   
+SET
       chatrooms.can_send_file = 0,
       chatrooms.update_user_id = 1,
       updated_at = NOW()
-WHERE 
+WHERE
       chatrooms.is_deleted = 0 AND chatroom_join_users.user_id != 1;
 COMMIT;
