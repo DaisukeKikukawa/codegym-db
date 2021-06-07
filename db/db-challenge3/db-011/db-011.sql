1d91a14df6c4725566ae926ae1004ac1e416c7dc
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-06-07 21:07:57 +0900

    db-011 SQL文 修正

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -4,6 +4,6 @@ UPDATE
 SET
       is_finished = 1,
       updated_user_id = 1,
-      created_at = NOW()
+      updated_at = NOW()
 WHERE created_at BETWEEN '2020-04-26 09:00:00' AND '2020-04-26 11:30:00' AND is_deleted = 0;
 COMMIT;
