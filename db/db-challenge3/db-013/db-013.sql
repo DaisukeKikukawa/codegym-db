577edb489dc3c28746ff81e2eb854e533dbc6222
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-06-07 21:22:21 +0900

    db-013 SQL文 修正

--- db/db-challenge3/db-013/db-013.sql
+++ db/db-challenge3/db-013/db-013.sql
@@ -1,22 +1,5 @@
 BEGIN;
 UPDATE
-  users
-SET
-  is_deleted = 1,
-  updated_at = NOW()
-WHERE
-  (
-    work_tell IS NULL
-    OR work_tell = ''
-  )
-  AND (
-    my_tell IS NULL
-    OR my_tell = ''
-  );
-COMMIT;
-
-BEGIN;
-UPDATE
       users
 SET
       is_deleted = 1,
